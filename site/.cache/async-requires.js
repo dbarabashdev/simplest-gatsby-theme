94f4087fa44345f57e37d657cf1046b61aea2642
dbarabashdev <dbarabashdev@gmail.com>
2020-01-11 16:32:26 +0200

    site folder in live

--- site/.cache/async-requires.js
+++ site/.cache/async-requires.js
@@ -2,6 +2,8 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
+  "component---gatsby-theme-simplest-src-templates-events-js": () => import("./../../gatsby-theme-simplest/src/templates/events.js" /* webpackChunkName: "component---gatsby-theme-simplest-src-templates-events-js" */),
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---gatsby-theme-simplest-src-templates-event-js": () => import("./../../gatsby-theme-simplest/src/templates/event.js" /* webpackChunkName: "component---gatsby-theme-simplest-src-templates-event-js" */)
 }
 
